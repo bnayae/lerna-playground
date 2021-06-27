8cdcb565945be3cde8762468d612db937a5c16b4
bnaya eshet <bnayanet@hotmail.com>
2021-06-27 09:08:41 +0300

    [ADD] 2 next projects

---
+++ packages/app-a/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>App A</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          App A
+        </h1>
+      </main>
+    </div>
+  )
+}
